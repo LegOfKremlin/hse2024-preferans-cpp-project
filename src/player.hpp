3c2d60192c735c8f670c9856b2137a4557f9b6fa
Дмитрий Русанов <99138978+DimaRus05@users.noreply.github.com>
2024-05-12 22:34:13 +0300

    Update player.hpp

--- src/player.hpp
+++ src/player.hpp
@@ -12,6 +12,7 @@ class Player {
 public:
     std::string name;
     int score;
+    int trickCount;
     int bullet;
     int mountain;
     int whist;
@@ -22,7 +23,13 @@ public:
 
     void discard();
     Card* playCard();
+    void showCards();
     bool decideWhist();
+	void incrementTrickCount();
+	int getTrickCount();
+	void awardPoints();
+	void penalizePoints(int trickCount);
+	void resetTrickCount();
     void revealCards();
     TradeResult makeBid(TradeResult highestBid, bool isFirstBid);
     int makeMove();
