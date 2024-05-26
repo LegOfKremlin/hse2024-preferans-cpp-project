31a1875e0f0b0d7e6092f78f1c835df38c042329
Дмитрий Русанов <99138978+DimaRus05@users.noreply.github.com>
2024-05-26 22:01:20 +0300

    Update typing.hpp

--- src/typing.hpp
+++ src/typing.hpp
@@ -1,50 +1,50 @@
 #ifndef TYPIING_HPP_
 #define TYPIING_HPP_
 
-#include <string>
+namespace godot
+{
 
-enum class Move {
-    Bidding,
-    Passout,
-    Misere,
-};
+    enum class Move {
+        Bidding,
+        Passout,
+        Misere,
+    };
 
+    enum class TradeResult {
+        Passout,
+        Misere,
+        MisereWithoutTalon,
+        SixSpades,
+        SixClubs,
+        SixDiamonds,
+        SixHearts,
+        SixNoTrump,
+        SevenSpades,
+        SevenClubs,
+        SevenDiamonds,
+        SevenHearts,
+        SevenNoTrump,
+        EightSpades,
+        EightClubs,
+        EightDiamonds,
+        EightHearts,
+        EightNoTrump,
+        NineSpades,
+        NineClubs,
+        NineDiamonds,
+        NineHearts,
+        NineNoTrump,
+        TenSpades,
+        TenClubs,
+        TenDiamonds,
+        TenHearts,
+        TenNoTrump,
+        NineSpadesWithoutTalon,
+        NineNoTrumpWithoutTalon,
+        TenSpadesWithoutTalon,
+        TenNoTrumpWithoutTalon
+    };
 
-enum class TradeResult {
-    Passout,
-    Misere,
-    MisereWithoutTalon,
-    SixSpades,
-    SixClubs,
-    SixDiamonds,
-    SixHearts,
-    SixNoTrump,
-    SevenSpades,
-    SevenClubs,
-    SevenDiamonds,
-    SevenHearts,
-    SevenNoTrump,
-    EightSpades,
-    EightClubs,
-    EightDiamonds,
-    EightHearts,
-    EightNoTrump,
-    NineSpades,
-    NineClubs,
-    NineDiamonds,
-    NineHearts,
-    NineNoTrump,
-    TenSpades,
-    TenClubs,
-    TenDiamonds,
-    TenHearts,
-    TenNoTrump,
-    NineSpadesWithoutTalon,
-    NineNoTrumpWithoutTalon,
-    TenSpadesWithoutTalon,
-    TenNoTrumpWithoutTalon
-};
-
-std::string tradeResultToString(TradeResult result);
+} // namespace godot
 
 #endif // TYPIING_HPP_
